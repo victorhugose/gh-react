7de079f68bff8630b0219205cb3b8d46f848bd50
Victor Sotelo <victorhugo.sotelo.e@gmail.com>
2023-05-24 20:03:55 -0500

    fix bug 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://victorhugose.github.io/gh-react-vite/',
+  base: 'https://victorhugose.github.io/gh-react/',
   plugins: [react()],
 })
